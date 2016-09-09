3224ad4865a548106f91a65213ef7bc252a1061a
Domonique Baul <dbaul1920@gmail.com>
2016-09-09 16:22:09 -0400

    app skeleton in comments

--- src/factorialCal.java
+++ src/factorialCal.java
@@ -3,7 +3,15 @@ public class factorialCal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		//Greeting
+		
+		//user input
+		
+		//call function method
+		
+		// Do you want to continue loop
+		
 	}
 
 }
