7223f7b75a6821b97855f3e3bb9190c5f43fa83b
Domonique Baul <dbaul1920@gmail.com>
2016-09-09 17:32:24 -0400

    add method

--- src/factorialCal.java
+++ src/factorialCal.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class factorialCal {
 
@@ -5,13 +6,28 @@ public class factorialCal {
 		// TODO Auto-generated method stub
 		
 		//Greeting
+		System.out.println("Welcome to the Factorial Calculator!");
 		
 		//user input
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter an integer that's greater than 0 but less than 10: ");
+		int x = input.nextInt();
 		
 		//call function method
-		
+		System.out.println(facMe(x));
 		// Do you want to continue loop
 		
 	}
 
+	// method goes here
+	public static int facMe(int x){
+		 
+		if(x==1){
+			return 1;	
+		}else{
+			return x*(x-1);
+		}
+	}
+
+	
 }
